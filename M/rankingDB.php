338397db00f558e44fceef3160e9f1d613438980
Kryddd <32571260+Kryddd@users.noreply.github.com>
2018-03-10 16:09:04 +0100

    end ranking page

--- M/rankingDB.php
+++ M/rankingDB.php
@@ -1,13 +1,15 @@
 <?php
-	function rankingDB() {
-		require('M/connectDB.php');
-		$resultQuery = 'SELECT username, scoreTotal FROM utilisateur ORDER BY scoreTotal DESC';
-		
-		$prepResult = $dbh->prepare($resultQuery);
-		$prepResult->execute();
-		
-		$ranking = $prepResult->fetchAll();
-		
-		return $ranking;
-	}
-?>
+function rankingDB() {
+    require('M/connectDB.php');
+    $resultQuery = 'SELECT U.username, J.score, P.nomPartie, J.idPartie 
+    FROM joue J, utilisateur U, partie P
+    WHERE U.idUtilisateur = J.idUtilisateur
+    AND P.idPartie = J.idPartie
+    ORDER BY P.nomPartie, J.score DESC';
+
+    $prepResult = $dbh->prepare($resultQuery);
+    $prepResult->execute();
+
+    $ranking = $prepResult->fetchAll();
+    return $ranking;
+}
